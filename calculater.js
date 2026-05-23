d3e9fd0611e27650004b58e03b04e6c002dc26b2
Chris Kunda <christianira2003@gmail.com>
2026-05-23 17:50:17 +0200

    sum

---
+++ calculater.js
@@ -0,0 +1,19 @@
+const  addTwoAndSeven = () => {
+  return 2 + 7;
+}
+
+console.log(addTwoAndSeven());
+
+// -----------------------------------------------------------------
+const addThreeAndFour = () => {
+  return 3 + 4;
+}
+
+console.log(addThreeAndFour());
+
+// -----------------------------------------------------------------
+function calculateSum(num1, num2) {
+ return num1 + num2;  
+}
+
+console.log(calculateSum(2,5));

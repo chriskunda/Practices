e971b2cd71e6a61d49d40ec41de48a63bfbb43a8
Chris Kunda <christianira2003@gmail.com>
2026-05-23 19:29:46 +0200

    done

---
+++ maskedemail.js
@@ -0,0 +1,17 @@
+const maskEmail = (email) => {
+  const atIndex = email.indexOf("@");
+
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+
+  const masked =
+    username[0] +
+    "*".repeat(username.length - 2) +
+    username[username.length - 1];
+
+  return masked + domain;
+};
+
+const email = "myemail@gmail.com";
+
+console.log(maskEmail(email));

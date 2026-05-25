a09289d8f3ccd468bdcbd7b935feee5f61c99639
Chris Kunda <christianira2003@gmail.com>
2026-05-25 11:47:59 +0200

    turebe

--- cards.js
+++ cards.js
@@ -13,3 +13,16 @@ function cardCounter(card) {
 }
 
 console.log(cardCounter(90));
+
+const isLeapYear = (check) => {
+  if ((check % 400 === 0) || (check % 100 !== 0 && check % 4 === 0)) {
+    return `${check} is a leap year.`;
+  } else {
+    return `${check} is not a leap year.`;
+  }
+}
+
+let year = 2000;
+let result = isLeapYear(year);
+
+console.log(result);

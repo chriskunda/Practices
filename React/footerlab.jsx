3d8cc9be798b70f85fcd311bd167176c8b66917a
Chris Kunda <christianira2003@gmail.com>
2026-06-04 11:06:18 +0200

    starting react

---
+++ React/footerlab.jsx
@@ -0,0 +1,26 @@
+export const Footer = () =>{
+  return(
+    <footer>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <a href="#">First</a>
+      <a href="#">Second</a>
+      <a href="#">Third</a>
+
+      <p>CopyRights reserved ©</p>
+    </footer>
+  )
+}

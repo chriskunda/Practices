c43168d2aa5a26bda39e283e68cfe69ddb9b1385
Chris Kunda <christianira2003@gmail.com>
2026-06-08 00:10:28 +0200

    toggleText

---
+++ React/toggletext.jsx
@@ -0,0 +1,12 @@
+const { useState } = React;
+
+export const ToggleApp = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <div id="toggle-container">
+      <button id="toggle-button">Message</button>
+      {isVisible && <p id="message">I love freeCodeCamp!</p>}
+    </div>
+  );
+};

35806d9f0bd7aa093023c153d94a1bb8515cdbe9
Chris Kunda <christianira2003@gmail.com>
2026-06-08 00:29:21 +0200

    toggle done

--- React/toggletext.jsx
+++ React/toggletext.jsx
@@ -1,11 +1,15 @@
 const { useState } = React;
 
 export const ToggleApp = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleToggleVisibility = () => {
+    setIsVisible(!isVisible);
+  }
 
   return (
     <div id="toggle-container">
-      <button id="toggle-button">Message</button>
+      <button onClick={handleToggleVisibility} id="toggle-button">{isVisible? "Hide " : "Show "}Message</button>
       {isVisible && <p id="message">I love freeCodeCamp!</p>}
     </div>
   );
